a8c9018cde0467d995946d7e5b29ae7592cbd8ab
HarwisarGheorghian <ggheorghian46@ladueschools.net>
2023-01-08 17:41:18 +0000

    initial commit

---
+++ Horse.java
@@ -0,0 +1,9 @@
+public class Horse{
+    private String name;
+    private int weight;
+
+    public Horse(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+}
---
+++ RandomPermRunner.java
@@ -0,0 +1,9 @@
+public class RandomPermRunner
+{
+  public static void main(String[] args)
+  {
+    int[] randNums = RandomPermutation.next(10);
+     
+    System.out.println(java.util.Arrays.toString(randNums));
+  }
+}
